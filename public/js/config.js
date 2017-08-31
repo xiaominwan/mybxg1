811383a3b8dcb84662e1120ccb9ab3514db502be
code1031 <abc@163.com>
2017-08-31 09:44:05 +0800

    讲师查看功能

--- public/js/config.js
+++ public/js/config.js
@@ -7,8 +7,15 @@ require.config({
         jquery:"jquery/jquery",
         cookie:"jquery-cookie/jquery.cookie",
         template:"artTemplate/template-web",
+        bootstrap:"bootstrap/js/bootstrap.min",
         common:"../js/common",
         login:"../js/login",
-        teacherlist:"../js/teacher-list"
+        teacherlist:"../js/teacher-list",
+
+    },
+    shim:{
+        bootstrap:{
+            deps:["jquery"]
+        },
     }
 });
--- public/js/teacher-list.js
+++ public/js/teacher-list.js
@@ -1,17 +1,41 @@
 /**
  * Created by Administrator on 2017/8/30 0030.
  */
-define(["jquery","template"],function($,template){
+define(["jquery","template","bootstrap"],function($,template){
     //调用后台接口获取列表数据
 
     $.ajax({
         type:"get",
         url:"/api/teacher",
         dataType:"json",
-        success:function(data){
-        var html=template("teacherTpl",{list:data.result});
+        success:function(data) {
+            var html = template("teacherTpl", {list: data.result});
             $("#teacherInfo").html(html);
+            //预览单机事件
+            $(".preview").click(function () {
+                var td = $(this).closest("td");
+                var tcId = td.attr("data-tcId");
+                //根据id查询数据
+                $.ajax({
+                    type: "get",
+                    url: "/api/teacher/view",
+                    data:{tc_id:tcId},
+                    dataType: "json",
+                    success: function (data) {
+                        console.log(data);
+                        //解析数据渲染页面
+                        var html = template("modalTpl", data.result);
+                        $("#modalInfo").html(html);
+                        //显示弹窗
+                        $("#teacherModal").modal();
+                    },
+
+
+                })
+            })
+
         }
 
-    });
-});
+    })
+
+})
