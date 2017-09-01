5557a06b5607abc2280be89165cd151f43406828
code1031 <abc@163.com>
2017-09-01 15:36:18 +0800

    添加表单验证

--- public/js/teacher-add.js
+++ public/js/teacher-add.js
@@ -1,4 +1,4 @@
-define(["jquery","template","util","datepicker","language"],function($,template,util){
+define(["jquery","template","util","datepicker","language","validate"],function($,template,util){
     //z设置菜单选中
     util.setMenu("/teacher/list");
     var tcId=util.qs("tc_id");
@@ -29,20 +29,43 @@ define(["jquery","template","util","datepicker","language"],function($,template,
     }
     //提交公用表单
     function submitForm(url){
-        $("#teacherBtn").click(function(){
-            $.ajax({
-                type:"post",
-                url:url,
-                data:$("#teacherForm").serialize(),
-                dataType:"json",
-                success:function(data){
-
-                    if(data.code==200){
-                        location.href="/teacher/list"
-                    }
+        $("#teacherForm").validate({
+            sendForm:false,
+            valid:function(){
+                console.log("ok");
+            },
+            description:{
+                tc_name:{
+                    required:"请输入用户名",
+                    valid:"用户名可以使用",
+                },
+                tc_pass:{
+                    required:"输入密码",
+                    pattern:"密码必须是六位数字",
+                    valid:"密码有效",
+                },
+                tc_join_date:{
+                    required:"请输入日期",
+                    valid:"密码有效",
                 }
-            });
+            }
         });
-
     }
+    //function submitForm(url){
+    //    $("#teacherBtn").click(function(){
+    //        $.ajax({
+    //            type:"post",
+    //            url:url,
+    //            data:$("#teacherForm").serialize(),
+    //            dataType:"json",
+    //            success:function(data){
+    //
+    //                if(data.code==200){
+    //                    location.href="/teacher/list"
+    //                }
+    //            }
+    //        });
+    //    });
+    //
+    //}
 });
